b2addbb7fcc6caf869a38e5718065ebdfc7fbc7b
konglili <konglili@zuoyebang.com>
2022-11-13 21:49:36 +0800

    test: lint

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import App from '@pages/App';
-const a = 11;
 console.log('232');
-console.log(b);
+console.log(fffff);
 createRoot(document.querySelector('#main')!).render(<App />);
