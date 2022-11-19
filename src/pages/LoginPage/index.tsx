263021f91a11aaa1e375eb024c9d2f4642696220
konglili <konglili@zuoyebang.com>
2022-11-19 17:51:35 +0800

    feat: 新增px-to-vm 插件

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,4 +1,21 @@
+import '@assets/styles/loginPage.css';
+
 function LoginPage(): JSX.Element {
-  return <div className="container">登陆模块</div>;
+  return (
+    <div className="h-full">
+      <div className=" flex flex-col items-center  justify-between box_login">
+        <div className="logo-box_login flex-initial items-center justify-center">
+          <div>
+            {/* <img src="" /> */}
+            <p className="text-12 leading-16  text-center">人的聚集地</p>
+          </div>
+        </div>
+        <div>
+          <div>接受该协议</div>
+          <p>注册</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default LoginPage;
