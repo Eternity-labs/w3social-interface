42733c2dfe70cee4726b56e28268a8102aad9b29
谭璇 <tanxuan_2013@163com>
2022-11-25 19:00:19 +0800

    feat: article

---
+++ src/components/Did/DidArticleCard.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+
+function DidArticleCard() {
+  return (
+    <div className="flex flex-col justify-between p-[12px] mb-[12px] rounded-[8px] bg-fSelect">
+      <h4 className="text-[12px] truncate mb-[28px]">
+        【全职远程】UI/UX设计师/酒店管理+环保创新平酒店管理+环保创新平
+      </h4>
+      <div className="flex items-center">
+        <Avatar className="text-[20px] mr-[8px]">H</Avatar>
+        <span className="text-[10px] text-[#363636]">次身份用户名</span>
+      </div>
+    </div>
+  );
+}
+
+export default DidArticleCard;
--- src/pages/MainPage/didDetail.tsx
+++ src/pages/MainPage/didDetail.tsx
@@ -10,6 +10,7 @@ import { styled } from '@mui/material/styles';
 import LocalFooter from '@components/Base/LocalFooter';
 import WishList from '@components/Did/WishList';
 import ButtonActions from '@components/Did/BottomActions';
+import DidArticleCard from '@components/Did/DidArticleCard';
 
 const CusTabList = styled(TabList)({
   '& .MuiTab-root': {
@@ -27,7 +28,7 @@ function DidDetail() {
   const { tabIndex, handleTabIndexChange } = useDidDetail();
 
   return (
-    <div className="p-[16px] pb-[48px] bg-green relative">
+    <div className="p-[16px] pb-[60px] bg-green relative">
       <div className="flex justify-between items-center">
         <ArrowBackIosIcon />
         <span className="flex justify-center items-center rounded-full px-[6px] py-[3px] text-[8px] color-fSelect bg-white">
@@ -40,11 +41,22 @@ function DidDetail() {
           <Tab className="pb-0 px-0 min-w-[40px]" label="自我介绍" value="1" />
           <Tab className="pb-0 px-0 min-w-[40px]" label="帖子" value="2" />
         </CusTabList>
-        <TabPanel className="h-[180px]" value="1">
-          Item One
+        <TabPanel className="h-[180px] p-0 my-[12px] overflow-hidden overflow-y-auto" value="1">
+          <div>
+            内容填字符，栏收录该内容. 10 篇文章 2 订阅. 订阅专栏. 常规类型的格式化.
+            String类的format()方法用于创建格式化的字符串以及连接多个字符串对象内容填字符，栏收录该内容.
+            10 篇文章 2 订阅. 订阅专栏. 常规类型的格式化.
+            String类的format()方法用于创建格式化的字符串以及连接多个字符串对象内容填字符，栏收录该内容.
+            10 篇文章 2 订阅. 订阅专栏. 常规类型的格式化.
+            String类的format()方法用于创建格式化的字符串以及连接多个字符串对象内容填字符，栏收录该内容.
+            10 篇文章 2 订阅. 订阅专栏. 常规类型的格式化.
+            String类的format()方法用于创建格式化的字符串以及连接多个字符串对象
+          </div>
         </TabPanel>
-        <TabPanel className="h-[180px]" value="2">
-          Item Two
+        <TabPanel className="h-[180px] p-0 my-[12px] overflow-hidden overflow-y-auto" value="2">
+          <DidArticleCard />
+          <DidArticleCard />
+          <DidArticleCard />
         </TabPanel>
       </TabContext>
       <LocalFooter />
