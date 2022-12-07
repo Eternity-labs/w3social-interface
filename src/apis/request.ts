4af9d937ed6cae17a0d48c326d792325e9673fbe
konglili <konglili@zuoyebang.com>
2022-12-07 21:06:49 +0800

    fix: request 带token认证完成

--- src/apis/request.ts
+++ src/apis/request.ts
@@ -3,27 +3,40 @@ import axios from 'axios';
 import type { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 import { handleGeneralError } from './tools';
 
+const tokenUrl = [
+  '/w3social/login/register',
+  '/w3social/login/changePassword',
+  '/w3social/login/login',
+];
 class Request {
   public instance: AxiosInstance;
 
   constructor(config: AxiosRequestConfig) {
     this.instance = axios.create(config);
     // 前置拦截器
-    // this.instance.interceptors.request.use(
-    //   (res: AxiosRequestConfig) => {
-    //     console.log('全局请求拦截器');
-    //     return res;
-    //   },
-    //   (err: any) => {
-    //     return Promise.reject(err);
-    //   }
-    // );
+    this.instance.interceptors.request.use(
+      (res: AxiosRequestConfig) => {
+        const token = localStorage.getItem('w3SocialThoen');
+        if (token) {
+          res.headers = {
+            Authorization: token,
+          };
+        }
+        return res;
+      },
+      (err: any) => {
+        return Promise.reject(err);
+      }
+    );
     this.instance.interceptors.response.use(
       // 因为我们接口的数据都在res.data下，所以我们直接返回res.data
       (res: AxiosResponse) => {
         const { data, code, message } = res.data;
         if (code !== 200) {
           handleGeneralError(code, message);
+        }
+        if (tokenUrl.indexOf(res.config.url || '') > 0) {
+          localStorage.setItem('w3SocialThoen', data.token);
         }
         return res.data;
       },
