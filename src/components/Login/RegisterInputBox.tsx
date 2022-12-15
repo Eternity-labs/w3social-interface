f78aea8a871eb4f1cecfebeafbaa60f03120ce87
konglili <konglili@zuoyebang.com>
2022-12-15 15:51:56 +0800

    fix: 倒计时优化

--- src/components/Login/RegisterInputBox.tsx
+++ src/components/Login/RegisterInputBox.tsx
@@ -4,8 +4,11 @@ import MuiButton from '@mui/material/Button';
 import { useRef, useImperativeHandle, useState, forwardRef } from 'react';
 import ErrorTip from '@components/Base/ErrorTip';
 import LoginServices from '@apis/services/LoginService';
+import { useMutation } from 'react-query';
+import { useCountDown } from 'ahooks';
 import InputCom from './Input';
-
+// 2min倒计时
+const COUNTDOWN = 2 * 60 * 1000;
 const emialReg = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 type InputProps = {
   onRef: any;
@@ -16,11 +19,20 @@ function RegisterBox(props: InputProps, ref: any) {
   const emailRef = useRef<HTMLInputElement>();
   const passRef = useRef<HTMLInputElement>();
   const vertifyPassRef = useRef<HTMLInputElement>();
-
+  const [targetDate, setTargetDate] = useState<number>();
+  const [countdown] = useCountDown({
+    targetDate,
+    onEnd: () => {},
+  });
   const [errorObj, setError] = useState<any>({
     email: '',
     pass: '',
     vertifyPass: '',
+  });
+  const SendCodeMutation = useMutation(LoginServices.sendCode, {
+    onSuccess: () => {
+      setTargetDate(Date.now() + COUNTDOWN);
+    },
   });
   let errorData = {
     email: '',
@@ -81,7 +93,7 @@ function RegisterBox(props: InputProps, ref: any) {
     checkEmail();
     setError(errorData);
     if (!errorData.email) {
-      const res = await LoginServices.sendCode({
+      SendCodeMutation.mutate({
         loginInfo: {
           email: emailRef.current!.value,
         },
@@ -102,15 +114,37 @@ function RegisterBox(props: InputProps, ref: any) {
     }
     return null;
   };
-  const endAdornmentCom = (
-    <MuiButton
-      onClick={getEmailCode}
-      variant="contained"
-      className=" w-[88px] h-[30px] text-white rounded-full bg-black text-[12px]"
-    >
-      获取验证码
-    </MuiButton>
-  );
+  const endAdornmentCom = () => {
+    if (SendCodeMutation.isLoading) {
+      return (
+        <MuiButton
+          variant="contained"
+          className=" w-[88px] h-[30px] text-white rounded-full bg-slimGray text-[12px]"
+        >
+          正在发送中...
+        </MuiButton>
+      );
+    }
+    if (countdown) {
+      return (
+        <MuiButton
+          variant="contained"
+          className=" w-[88px] h-[30px] text-white rounded-full bg-slimGray text-[12px]"
+        >
+          {Math.round(countdown / 1000)}s
+        </MuiButton>
+      );
+    }
+    return (
+      <MuiButton
+        onClick={getEmailCode}
+        variant="contained"
+        className=" w-[88px] h-[30px] text-white rounded-full bg-black  text-[12px]"
+      >
+        获取验证码
+      </MuiButton>
+    );
+  };
   useImperativeHandle(onRef, () => {
     // 需要将暴露的接口返回出去
     return {
@@ -124,7 +158,7 @@ function RegisterBox(props: InputProps, ref: any) {
         placeholder="输入邮箱"
         className="mt-[64px] pr-[4px]"
         InputRef={emailRef}
-        endAdornmentCom={endAdornmentCom}
+        endAdornmentCom={endAdornmentCom()}
       >
         <MailOutlineIcon />
       </InputCom>
